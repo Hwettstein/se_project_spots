4f0df955e2e6e80cadb79e377b42eda25303a08c
Howard Wettstein <hosurendal@gmail.com>
2025-02-05 10:56:41 -0600

    Test changes for project-9 branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+//Project 9 Webpack Config test commit
+
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
